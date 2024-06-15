bddfb3193be04905d2fc00c9296780a4f0dd5bd2
hαrsh vαirαgi <100824266+OurCodeBase@users.noreply.github.com>
2024-06-15 04:50:12 +0000

    myself, projectList and photos has been updated.

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/desk-workspace.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "code-editor",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A code editor built on top of neovim combines the powerful, customizable, and efficient core of neovim with a modern, user-friendly interface and advanced features.",
+    url: "https://github.com/OurCodeBase/code-editor",
   },
   {
-    title: "Web Development for Beginners",
+    title: "bash-sdk",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A bash library to create standalone scripts.",
+    url: "https://github.com/OurCodeBase/bash-sdk",
   },
   {
-    title: "My Resume Site",
+    title: "bashsdk-docs",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "bashsdk-docs (docs for bash-sdk) is hosted on gitlab using CI/CD Server and built using VitePress and markdown.",
+    url: "https://gitlab.com/OurCodeBase/bashsdk-docs",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "friendly-snippets",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Contribute in the best set of snippets available for neovim.",
+    url: "https://github.com/OurCodeBase/friendly-snippets",
   },
 ];
 
